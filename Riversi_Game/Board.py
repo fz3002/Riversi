5a45cf4d002435c8d804c8061367173800b65d5b
fz3002 <franc3002@gmail.com>
2024-05-26 14:36:08 +0200

    Add Board drawing

--- Riversi_Game/Board.py
+++ Riversi_Game/Board.py
@@ -1,19 +1,35 @@
 import tkinter as tk
 
-board = [[None]*10 for _ in range(10)]
+BOARD_SIZE = 400
 
-counter = 0
+WIDTH = 8
+FIELD_SIZE = BOARD_SIZE/WIDTH
+
+DISK_SIZE = int(0.8 * FIELD_SIZE)
+BORDER_SIZE = 5
 
 root = tk.Tk()
 
-root.title("Reversi")
-root.geometry('100x100')
+root.title("Checkers")
+root.geometry(f'{BOARD_SIZE}x{BOARD_SIZE}+100+100')
+root.resizable(0,0)
+
+board = tk.Frame(root)
+board.pack()
+
+draughts = tk.Canvas(board, width = BOARD_SIZE, height = BOARD_SIZE, borderwidth=15, background='#6e3a00')
+draughts.pack(fill = 'both')
+
+
+for row in range(WIDTH + 1):
+    for column in range(WIDTH + 1):
+        
+        draught = draughts.create_rectangle(row*FIELD_SIZE,column*FIELD_SIZE,\
+                                            (row*FIELD_SIZE)+FIELD_SIZE,(column*FIELD_SIZE)+FIELD_SIZE,\
+                                            fill = '#026e00', outline = 'black')
+
 
 # TODO: click handler
 
-for i, row in enumerate(board):
-    for j, column in enumerate(row):
-        L = tk.Label(root, text='    ', bg='green', width=int(100/len(board)), height=int(100/len(board)), borderwidth=1, relief="solid")
-        L.grid(row=i, column=j)
 
 root.mainloop()
