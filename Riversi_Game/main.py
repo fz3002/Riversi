0046e3bf23f74efa428e1f48ddcf7695ae99a444
fz3002 <franc3002@gmail.com>
2024-06-06 19:43:52 +0200

    Add menu page before game

--- Riversi_Game/main.py
+++ Riversi_Game/main.py
@@ -7,16 +7,25 @@ import tkinter as tk
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        
-        #TODO: menu
 
+        # TODO: menu
         self.title = "Riversi Game"
-
-        m = board.Board()
-        v = view.View(self)
+        self.wm_geometry("432x510")
+        self.m = board.Board()
+        self.v = view.View(self)
+        self.menu = view.Menu(self)
+        self.show_menu()
         self.resizable(False, False)
-        cont = controller.Controller(m, v)
-        v.set_controller(cont)
+        self.cont = controller.Controller(self.m, self.v)
+        self.v.set_controller(self.cont)
+
+    def show_game(self):
+        self.menu.pack_forget()
+        self.v.pack(expand=True, fill="both")
+
+    def show_menu(self):
+        self.v.pack_forget()
+        self.menu.pack(expand=True, fill="both")
 
 
 app = App()
--- Riversi_Game/view.py
+++ Riversi_Game/view.py
@@ -1,14 +1,29 @@
 import tkinter as tk
 from tkinter import messagebox
+
+from numpy import pad
 from controller import Controller
 
 
 class View(tk.Frame):
     def __init__(self, parent, board_size=400, column_count=8):
+        self.root = parent
         super().__init__(parent, borderwidth=15, background="#6e3a00")
-        self.current_player_label = tk.Label(parent, text="Player: black")
-        self.current_player_label.pack()
         self.pack()
+        self.button_frame = tk.Frame(self)
+        self.button_to_main_window = tk.Button(
+            self, text="Go Back To Menu", command=self.root.show_menu
+        )
+        self.button_to_main_window.pack()
+        self.current_player_label = tk.Label(
+            self,
+            text="Player: black",
+            background="#6e3a00",
+            foreground="Black",
+            font=("BigBlueTerm437 Nerd Font", 25),
+            pady=10,
+        )
+        self.current_player_label.pack()
         self.board_size = board_size
         self.column_count = column_count
         self.field_size = self.board_size / self.column_count
@@ -26,13 +41,14 @@ class View(tk.Frame):
 
         self.border_size = 5
 
-        self.root = parent
-
-        self.controller : Controller
+        self.controller: Controller
 
         self.draw_board()
         self.fields.bind("<Button-1>", self.mouse_click_handler)
         self.create_start_disks()
+
+    def show(self):
+        self.lift()
 
     def set_controller(self, controller):
         self.controller = controller
@@ -53,7 +69,7 @@ class View(tk.Frame):
         self.fields.delete("tile")
         for x in range(self.column_count):
             for y in range(self.column_count):
-                if board[x][y] is not "":
+                if board[x][y] != "":
                     self.create_disk(x, y, board[x][y])
         self.fields.update()
 
@@ -87,10 +103,10 @@ class View(tk.Frame):
         return disk
 
     def set_current_player_label(self, player):
-        self.current_player_label.config(text="Player: " + player)
+        self.current_player_label.config(text="Player: " + player, foreground=player)
 
     def end_game_message(self, score):
-        score_text = "Black: " +  str(score[0]) +  "\nWhite: " + str(score[1])
+        score_text = "Black: " + str(score[0]) + "\nWhite: " + str(score[1])
         if score[0] > score[1]:
             score_text += "\nBlack Won !!!"
         elif score[0] < score[1]:
@@ -101,3 +117,89 @@ class View(tk.Frame):
 
     def pass_window(self):
         messagebox.showinfo("Pass window", "Pass")
+
+
+class Menu(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent, background="#6e3a00")
+        self.root = parent
+        self.pack(expand=True, fill="both")
+        self.label = tk.Label(
+            self,
+            text="Menu",
+            font=("BigBlueTerm437 Nerd Font", 25),
+            background="#6e3a00",
+            foreground="#ffe200",
+            pady = 10
+        )
+        self.label.pack()
+        self.button_play = tk.Button(
+            self,
+            text="Play",
+            command=self.root.show_game,
+            bg = "#026e00",
+            fg = "#ffe200",
+            activebackground = "#015400",
+            activeforeground = "#ffe200",
+            height=2,
+            width=20,
+            font=("BigBlueTerm437 Nerd Font", 20),
+            pady=5
+        )
+        self.button_play_vs_ai = tk.Button(
+            self,
+            text="Play vs AI",
+            command=self.root.show_game,
+            bg = "#026e00",
+            fg = "#ffe200",
+            activebackground = "#015400",
+            activeforeground = "#ffe200",
+            height=2,
+            width=20,
+            font=("BigBlueTerm437 Nerd Font", 20),
+            pady=5
+        )
+        self.button_save = tk.Button(
+            self,
+            text="Save",
+            command=self.root.show_game,
+            bg = "#026e00",
+            fg = "#ffe200",
+            activebackground = "#015400",
+            activeforeground = "#ffe200",
+            height=2,
+            width=20,
+            font=("BigBlueTerm437 Nerd Font", 20),
+            pady=5
+        )
+        self.button_load = tk.Button(
+            self,
+            text="Load",
+            command=self.root.show_game,
+            bg = "#026e00",
+            fg = "#ffe200",
+            activebackground = "#015400",
+            activeforeground = "#ffe200",
+            height=2,
+            width=20,
+            font=("BigBlueTerm437 Nerd Font", 20),
+            pady=5
+        )
+        self.button_exit = tk.Button(
+            self,
+            text="Exit",
+            command=self.root.destroy,
+            bg = "#026e00",
+            fg = "#ffe200",
+            activebackground = "#015400",
+            activeforeground = "#ffe200",
+            height=2,
+            width=20,
+            font=("BigBlueTerm437 Nerd Font", 20),
+            pady=5
+        )
+        self.button_play.pack()
+        self.button_play_vs_ai.pack()
+        self.button_save.pack()
+        self.button_load.pack()
+        self.button_exit.pack()
